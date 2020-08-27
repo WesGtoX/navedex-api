fb64b812bf4311ad85bf52832c5b84c43423520b
Wesley Mendes <wesley_gto_x@yahoo.com.br>
2020-08-27 09:59:41 -0300

    feat: update base routes

--- api/urls.py
+++ api/urls.py
@@ -1,40 +1,35 @@
 from django.contrib import admin
 from django.urls import path, include
-
 from rest_framework_simplejwt import views as jwt_views
 
 from . import routers
-from .views import redirect_view
-
 from users.views import UserViewSet
 
 from navers import urls as naver_url
 from projects import urls as project_url
 
 router = routers.CustomRouter()
-router.register(r'users', UserViewSet)
+router.register(r'signup', UserViewSet)
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/', include(router.urls)),
+    path('', include(router.urls)),
 
-    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('login/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 
-    path('api/v1/navers/store/', naver_url.naver_create, name='naver-create'),
-    path('api/v1/navers/index/', naver_url.naver_list, name='naver-list'),
-    path('api/v1/navers/show/<int:pk>/', naver_url.naver_detail, name='naver-detail'),
-    path('api/v1/navers/update/<int:pk>/', naver_url.naver_update, name='naver-update'),
-    path('api/v1/navers/delete/<int:pk>/', naver_url.naver_delete, name='naver-delete'),
+    path('navers/store/', naver_url.naver_create, name='naver-create'),
+    path('navers/index/', naver_url.naver_list, name='naver-list'),
+    path('navers/show/<int:pk>/', naver_url.naver_detail, name='naver-detail'),
+    path('navers/update/<int:pk>/', naver_url.naver_update, name='naver-update'),
+    path('navers/delete/<int:pk>/', naver_url.naver_delete, name='naver-delete'),
 
-    path('api/v1/projects/store/', project_url.project_create, name='project-create'),
-    path('api/v1/projects/index/', project_url.project_list, name='project-list'),
-    path('api/v1/projects/show/<int:pk>/', project_url.project_detail, name='project-detail'),
-    path('api/v1/projects/update/<int:pk>/', project_url.project_update, name='project-update'),
-    path('api/v1/projects/delete/<int:pk>/', project_url.project_delete, name='project-delete'),
+    path('projects/store/', project_url.project_create, name='project-create'),
+    path('projects/index/', project_url.project_list, name='project-list'),
+    path('projects/show/<int:pk>/', project_url.project_detail, name='project-detail'),
+    path('projects/update/<int:pk>/', project_url.project_update, name='project-update'),
+    path('projects/delete/<int:pk>/', project_url.project_delete, name='project-delete'),
 
     path('api-auth/', include('rest_framework.urls')),
-
-    path('', redirect_view),
 ]
--- api/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import redirect
-
-
-def redirect_view(request):
-    return redirect('api/v1/')
